836625d77c2273174690ecbbdb9374ca29fdcd5f
ZecongLiang <55116253+ZecongLiang@users.noreply.github.com>
2020-02-26 23:23:10 -0500

    Submitted Lab 05 File

--- Lab 05/lab05.c
+++ Lab 05/lab05.c
@@ -17,23 +17,27 @@
 *  Day, Time, Location of Lab: Thursday, 3:30 pm, SC 231
 *
 *  Program Description:
-*
+*  Calculates the force on a particle by other two particles with user inputted values.
 ***************************************************************************/
 #include <stdio.h>
 #include <math.h>
 
-#define K (8.99 * pow (10,9))
-#define PI 3.14159265358979323846264338327950
+#define K (8.99 * pow (10,9))   //coulomb's constant
+#define PI 3.14159265358979323846264338327950 //pi
 
+// Function declearation
 double microC (double q);
 double cm2meter (double coordinate);
 double cForce(double q1, double q2, double radius);
 double angle(double Fx,double Fy);
+double angleD(double Fx,double Fy);
 double resultForce(double Fx, double Fy);
 double distance(double xCord, double yCord);
-double orthogonalx(double angle, double cForce);
-double orthogonaly(double xCord, double yCord);
+double orthogonalx(double angle);
+double orthogonaly(double xCord);
+double orthogonalForce(double,double,double,double);
 
+//Main Program
 int main ()
 {
   double q1; //Input for point charge 1 (microCoulomb)
@@ -47,14 +51,18 @@ int main ()
   double angleF; //Angle of force vector with x-axis (degrees)
   double distance12; //Distance from point 1 to point 2;
   double distance13; //Distance from point 1 to point 3;
-  double orthogXComp2;
-  double orthogXComp3;
-  double orthogYComp2;
-  double orthogYComp3;
-  double angle12; //Angle of point 1 and point 2;
-  double angle13; //Angle of point 1 and point 3;
-  double orthogXCompF;
-  double orthogYCompF;
+  double orthogXComp2; //Orthogonal x direction for particle 2
+  double orthogXComp3; //Orthogonal x direction for particle 3
+  double orthogYComp2; //Orthogonal y direction for particle 2
+  double orthogYComp3; //Orthogonal y direction for particle 3
+  double angleRad12; //Angle of point 1 and point 2 in radians
+  double angleRad13; //Angle of point 1 and point 3 in radians
+  double angleDeg12; //Angle of point 1 and point 2 in degrees
+  double angleDeg13; //Angle of point 1 and point 3 in degrees
+  double orthogXCompF; //total orthogonal force component in x direction
+  double orthogYCompF; //total orthogonal force component in y direction
+  double force12; //Force between particel 1 and 2
+  double force13; //Force between particel 1 and 3
 
   // INPUT/OUTPUT
   printf("Enter charge [micro-Coulomb] of point #1 -> ");
@@ -84,29 +92,34 @@ int main ()
   // Function call to calculate values
   distance12 = distance(x2,y2);
   distance13 = distance(x3,y3);
-  angle12 = angle(x2,y2);
-  angle13 = angle(x3,y3);
+  angleRad12 = angle(x2,y2);
+  angleRad13 = angle(x3,y3);
+  angleDeg12 = angleD(x2,y2);
+  angleDeg13 = angleD(x3,y3);
 
-  resultF = resultForce(cForce(q1 , q2, distance12), cForce(q1 , q3, distance13));
-  angleF = angle(cForce(q1 , q2, distance12), cForce(q1 , q3, distance13));
+  force12 = cForce(q1 , q2, distance12);
+  force13 = cForce(q1 , q3, distance13);
 
-  orthogXComp2 = orthogonalx (angle12,cForce(q1 , q2, distance12));
-  orthogXComp3 = orthogonalx (angle13,cForce(q1 , q3, distance13));
-  orthogYComp2 = orthogonaly (angle12,cForce(q1 , q2, distance12));
-  orthogYComp3 = orthogonaly (angle13,cForce(q1 , q3, distance13));
+  orthogXComp2 = orthogonalx (angleRad12);
+  orthogXComp3 = orthogonalx (angleRad13);
+  orthogYComp2 = orthogonaly (angleRad12);
+  orthogYComp3 = orthogonaly (angleRad13);
 
-  orthogXCompF = orthogonalx(angleF, resultF);
-  orthogYCompF = orthogonalx(angleF, resultF);
+  orthogXCompF = orthogonalForce(orthogXComp2, orthogXComp3, force12, force13);
+  orthogYCompF = orthogonalForce(orthogYComp2, orthogYComp3, force12, force13);
+
+  resultF = resultForce(orthogXCompF, orthogYCompF);
+  angleF = angleD(orthogXCompF, orthogYCompF);
 
   //OUTPUT
-  printf("\n\nDistance [cm] and Angle [degrees] from point 1 to 2: %.1lf, %.1lf",distance12 * 100, angle12);
-  printf("\nDistance [cm] and Angle [degrees] from point 1 to 3: %.1lf, %.1lf",distance13 * 100,angle13);
+  printf("\nDistance [cm] and Angle [degrees] from point 1 to 2: %.1lf, %.1lf", distance12 * 100, angleDeg12);
+  printf("\nDistance [cm] and Angle [degrees] from point 1 to 3: %.1lf, %.1lf", distance13 * 100, angleDeg13);
 
-  printf("\n\nOrthogonal components point 1 to point 2 [x,y]: %.1lf, %.1lf",orthogXComp2,orthogYComp2);
-  printf("\nOrthogonal components point 1 to point 3 [x,y]: %.1lf, %.1lf",orthogXComp3,orthogYComp3);
+  printf("\n\nOrthogonal components point 1 to point 2 [x,y]: %.3lf, %.3lf", orthogXComp2, orthogYComp2);
+  printf("\nOrthogonal components point 1 to point 3 [x,y]: %.3lf, %.3lf", orthogXComp3, orthogYComp3);
 
-  printf("\n\nForce [N] of x and y direction: %.1lf, %.1lf", orthogXCompF, orthogYCompF);
-  printf("\nNet force [N] and angle [degrees]: %.1lf, %.1lf\n", resultF, angleF);
+  printf("\n\nForce [N] of x and y direction: %.3lf, %.3lf", orthogXCompF, orthogYCompF);
+  printf("\nNet force [N] and angle [degrees]: %.3lf, %.1lf\n", resultF, angleF);
 
   return(0);
 }
@@ -183,12 +196,33 @@ double cForce(double q1, double q2, double radius)
 *    2. double Fx, force due to the charge on the x-axis
 *
 *  Function Description:
-*  Calculates the angle of of the resultant force on the origin point charge
+*  Calculates the angle [radians] of of the resultant force on the origin point charge
 *  as a result of Fx and Fy
 ***************************************************************************/
 double angle(double Fy, double Fx)
 {
-  return atan(Fy / Fx) * 180 / PI;
+  return atan(Fx / Fy);
+}
+
+/***************************************************************************
+*
+*  Function Information
+*
+*  Name of Function: angleD
+*
+*  Function Return Type: double
+*
+*  Parameters (list data type, name, and comment one per line):
+*    1. double Fy, force due to the charge on the y-axis
+*    2. double Fx, force due to the charge on the x-axis
+*
+*  Function Description:
+*  Calculates the angle [degrees] of of the resultant force on the origin point charge
+*  as a result of Fx and Fy
+***************************************************************************/
+double angleD(double Fy, double Fx)
+{
+  return atan(Fx / Fy) * 180 / PI;
 }
 
 /***************************************************************************
@@ -224,18 +258,70 @@ double resultForce(double Fx, double Fy)
 *    2. double yCord, y-coordinate of the charge
 *
 *  Function Description:
-*  Claculates the magitude of the resultant force on the point charge
+*  Calculates the magitude of the resultant force on the point charge
 ***************************************************************************/
 double distance(double xCord, double yCord)
 {
-  return sqrt(pow(xCord,2)+pow(yCord,2));
+  return sqrt(pow(xCord,2) + pow(yCord,2));
 }
-double orthogonalx(double angle, double force)
+
+/***************************************************************************
+*
+*  Function Information
+*
+*  Name of Function: orthogonalx
+*
+*  Function Return Type: double
+*
+*  Parameters (list data type, name, and comment one per line):
+*    1.double, angle, angle in radian
+*
+*  Function Description:
+*  Finds the orthogonal components in the x direction.
+***************************************************************************/
+double orthogonalx(double angle)
 {
   return cos(angle);
 }
 
-double orthogonaly(double angle, double force)
+/***************************************************************************
+*
+*  Function Information
+*
+*  Name of Function: orthogonaly
+*
+*  Function Return Type: double
+*
+*  Parameters (list data type, name, and comment one per line):
+*    1.double, angle, angle in radian
+*
+*  Function Description:
+*  Finds the orthogonal components in the y direction.
+***************************************************************************/
+double orthogonaly(double angle)
 {
   return sin(angle);
+}
+
+/***************************************************************************
+*
+*  Function Information
+*
+*  Name of Function: orthogonalForce
+*
+*  Function Return Type: double
+*
+*  Parameters (list data type, name, and comment one per line):
+*     1.double, component12, the x/y component of the force bewteen particle 1 and 2
+*     2.double, component13, the x/y component of the force bewteen particle 1 and 3
+*     3.double, force, the x/y component of the resultant force
+*
+*  Function Description:
+*  Finds the orthogonal force component using the given forces and components.
+***************************************************************************/
+double orthogonalForce(double component12, double component13, double force12, double force13)
+{
+  double force; //orthogonal force
+  force = component12 * force12 + component13 * force13;
+  return(force);
 }
